b8a9c3d288e7c13bf38bb524dbdd6170ac89a610
Matías Duhalde <matias.duhalde@uc.cl>
2023-07-02 21:32:30 +0200

    fix: html paths

--- src/options/index.tsx
+++ src/options/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-export const Options = () => {
-  return <div>Options</div>;
+export const Options: React.FC = () => {
+  return (
+    <React.StrictMode>
+      <div>Options</div>
+    </React.StrictMode>
+  );
 };
+
+console.log('Create root');
 
 createRoot(document.getElementById('root')!).render(<Options />);
--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-export const Popup = () => {
-  return <div>Popup</div>;
+export const Popup: React.FC = () => {
+  return (
+    <React.StrictMode>
+      <div>Popup</div>
+      <div>Popup</div>
+      <div>Popup</div>
+      <div>Popup</div>
+      <div>Popup</div>
+      <div>Popup</div>
+      <div>Popup</div>
+      <div>Popup</div>
+      <div>Popup</div>
+      <div>Popup</div>
+      <div>Popup</div>
+      <div>Popup</div>
+      <div>Popup</div>
+    </React.StrictMode>
+  );
 };
+
+console.log('Create root');
 
 createRoot(document.getElementById('root')!).render(<Popup />);
